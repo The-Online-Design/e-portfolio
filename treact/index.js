c47374506499ef474b245198cf09d24a25e290cf
The-Online-Design <theonlinedesign@hotmail.com>
2022-07-12 12:54:51 -0400

    finalized portfolio and edited projects

---
+++ treact/index.js
@@ -0,0 +1,10 @@
+function openMenu (){
+    document.body.classList += "menu--open"
+}
+
+// if theres many classes already you need a space before menu
+
+function closeMenu (){
+    document.body.classList.remove('menu--open')
+}
+
